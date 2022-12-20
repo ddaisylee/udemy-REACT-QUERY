3aed55c928d366ebe8ccf03fe514cecad0da2ebc
ddaisylee <elliejay9988@gmail.com>
2022-12-20 23:22:52 +0900

    feat: useMutation 실습 - 포스트 삭제 기능 구현

--- base-blog-em/src/PostDetail.jsx
+++ base-blog-em/src/PostDetail.jsx
@@ -1,4 +1,4 @@
-import { useQuery } from "react-query";
+import { useMutation, useQuery } from "react-query";
 
 async function fetchComments(postId) {
   const response = await fetch(
@@ -30,13 +30,25 @@ export function PostDetail({ post }) {
     () => fetchComments(post.id)
   );
 
+  const deleteMutation = useMutation((postId) => deletePost(postId));
+
   if (isLoading) return <h1>로딩중...</h1>;
   if (isError) return <h1>에러 발생: {error.toString()}</h1>;
 
   return (
     <>
       <h3 style={{ color: "blue" }}>{post.title}</h3>
-      <button>Delete</button> <button>Update title</button>
+      <button onClick={() => deleteMutation.mutate(post.id)}>Delete</button>
+      {deleteMutation.isError && (
+        <p style={{ color: "red" }}>Error deleting the post</p>
+      )}
+      {deleteMutation.isLoading && (
+        <p style={{ color: "purple" }}>Deleting the post...</p>
+      )}
+      {deleteMutation.isSuccess && (
+        <p style={{ color: "green" }}>Post deleted!</p>
+      )}
+      <button>Update title</button>
       <p>{post.body}</p>
       <h4>Comments</h4>
       {data &&
