b6ace5409277fd66f43a44f63a6a5cfe6e91b763
ddaisylee <elliejay9988@gmail.com>
2022-12-21 23:49:43 +0900

    feat: useInfiniteQuery로 무한스크롤 구현

--- base-infinite-swapi/src/people/InfinitePeople.jsx
+++ base-infinite-swapi/src/people/InfinitePeople.jsx
@@ -1,4 +1,5 @@
 import InfiniteScroll from "react-infinite-scroller";
+import { useInfiniteQuery } from "react-query";
 import { Person } from "./Person";
 
 const initialUrl = "https://swapi.dev/api/people/";
@@ -8,6 +9,40 @@ const fetchUrl = async (url) => {
 };
 
 export function InfinitePeople() {
-  // TODO: get data for InfiniteScroll via React Query
-  return <InfiniteScroll />;
+  const {
+    data,
+    fetchNextPage,
+    hasNextPage,
+    isLoading,
+    isFetching,
+    isError,
+    error,
+  } = useInfiniteQuery(
+    "sw-people",
+    ({ pageParam = initialUrl }) => fetchUrl(pageParam),
+    {
+      getNextPageParam: (lastPage) => lastPage.next || undefined,
+    }
+  );
+
+  if (isLoading) return <div className="loading">로딩중...</div>;
+  if (isError) return <div>에러가 발생했습니다...{error.toString()}</div>;
+
+  return (
+    <>
+      {isFetching && <div className="loading">로딩중...</div>}
+      <InfiniteScroll loadMore={fetchNextPage} hasMore={hasNextPage}>
+        {data.pages.map((pageData) =>
+          pageData.results.map((person) => (
+            <Person
+              eyeColor={person.eye_color}
+              hairColor={person.hair_color}
+              name={person.name}
+              key={person.name}
+            />
+          ))
+        )}
+      </InfiniteScroll>
+    </>
+  );
 }
